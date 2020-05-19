1da4c41e8f6bf054a13f195cca0c7e3389676cf8
TadjievCodes <muhammad2008.08@mail.ru>
2020-05-19 00:45:39 -0400

    adding DOM code

---
+++ js/scripts.js
@@ -0,0 +1,36 @@
+// 1: Select the element with the ID 'about'. 
+//    Store the element in the variable `about`.
+const about = document.querySelector("#about");
+about.style.border = "2px solid firebrick";
+
+// 2: Select all the <h2> elements in the document.
+//    Set the color of the <h2> elements to a different color.
+ const colors = document.getElementsByTagName("h2");
+     //colors.style.color = "blue";
+     for(let i = 0; i < h2.length; i += 1);
+      h2[i].style.color = "slateblue";
+
+// 3: Select all elements with the class '.card'. 
+//    Set their background color to the color of your choice.
+const cards = document.getElemenetsByClassName("card");
+      for(let i = 0; i < cards.length; i += 1) {
+    card[i].style.backgroundColor = "seashell";
+      }
+
+// 4: Select only the first <ul> in the document.
+//    Assign it to a variable named `ul`.
+
+const ul = document.querySelector("ul");
+ul.style.border = "2px solid indigo";
+
+// 5: Select only the second element with the class '.container'.
+//    Assign it to a variable named `container`.
+const container = document.getElementByClassName("container")[1];
+container.style.backgroundColor = "royalblue";
+
+// 6: Select all <a> elements that have a 'title' attribute. 
+//    Set their color value to the color of your choice.
+const letters = document.querySelectorAll("a[title]");
+letters.style.color = "#dc2456";
+
+
